e62a0e88271171b1658ccf8cd4df745844242718
Namdak Tonpa <maxim@synrc.com>
2019-08-14 22:17:06 +0300

    disable p12

--- priv/ca_ecdsa/15.client.cert.sh
+++ priv/ca_ecdsa/15.client.cert.sh
@@ -1,6 +1,6 @@
 # generate client cert
-#openssl ca -config intermediate/server.cnf -extensions usr_cert -days 730 -in intermediate/csr/client_ecdsa.csr -out intermediate/certs/client_ecdsa_cert.pem
+openssl ca -config intermediate/server.cnf -extensions usr_cert -days 730 -in intermediate/csr/client_ecdsa.csr -out intermediate/certs/client_ecdsa_cert.pem
 
 # Combine Cert/Key, PKCS12:
-openssl pkcs12 -export -inkey intermediate/private/client_ecdsa_key.pem -in intermediate/certs/client_ecdsa_cert.pem -out intermediate/private/client_ecdsa.p12
+#openssl pkcs12 -export -inkey intermediate/private/client_ecdsa_key.pem -in intermediate/certs/client_ecdsa_cert.pem -out intermediate/private/client_ecdsa.p12
 
