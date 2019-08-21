2dc344be960f78b8509aa302f30102a44f1cf2c0
Namdak Tonpa <maxim@synrc.com>
2019-08-21 05:58:41 +0300

    clean certs

--- priv/ca_ecdsa/02.cert.sh
+++ priv/ca_ecdsa/02.cert.sh
@@ -1,2 +1,2 @@
 # generate root cert
-openssl req -config synrc.cnf -days 3650 -new -x509 -sha384 -extensions v3_ca -key private/ca.root.key.pem -out certs/ca.root.crt.pem
+openssl req -config synrc.cnf -days 3650 -new -x509 -sha384 -extensions v3_ca -key private/ca.root.key.pem -out certs/ca.root.crt.pem -trustout
