1472eb53d8cbd6641c66a14a06b8de292b0b94b6
Namdak Tonpa <maxim@synrc.com>
2019-08-28 18:02:27 +0300

    release

--- src/ca.erl
+++ src/ca.erl
@@ -5,6 +5,8 @@
 -export([start/2, stop/1, init/1]).
 
 start(_StartType, _StartArgs) ->
+   ca_enroll:boot("rsa"),
+   ca_enroll:boot("ecc"),
    R = cowboy_router:compile([{'_', [{"/:crypto/:nsCertType", ca_enroll, []}]}]),
    {ok, _} = cowboy:start_clear(http,[{port,8046}],#{env => #{dispatch => R}}),
    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
--- src/ca_enroll.erl
+++ src/ca_enroll.erl
@@ -3,36 +3,53 @@
 -compile(export_all).
 -export([init/2,create/2]).
 
-reply(X,Y,Z,R) -> cowboy_req:reply(X,Y,Z,R).
-bind(X,Y) -> cowboy_req:binding(X,Y).
+run(X)          -> sh:sh(sh:run(X)).
+reply(X,Y,Z,R)  -> cowboy_req:reply(X,Y,Z,R).
+bind(X,Y)       -> cowboy_req:binding(X,Y).
+template()      -> {ok,Bin} = file:read_file("priv/cnf/synrc.cnf"), Bin.
+replace(S,A,B)  -> re:replace(S,A,B,[global,{return,list}]).
+echo(<<>>, Req) -> reply(400,#{},<<"General Enrolment Error">>,Req);
+echo(Echo, Req) -> reply(200,#{<<"content-type">> => <<"text/plain;charset=utf-8">>},Echo,Req).
+
+boot(Crypto) ->
+   Temp    = template(),
+   Tem2    = replace(Temp,"PATH", mad_utils:cwd()),
+   Bin     = iolist_to_binary(replace(Tem2,"CRYPTO",Crypto)),
+   Gen     = lists:concat(["cert/",Crypto,"/synrc.cnf"]),
+   Index   = lists:concat(["cert/",Crypto,"/index.txt"]),
+   CRL     = lists:concat(["cert/",Crypto,"/crlnumber"]),
+   Serial  = lists:concat(["cert/",Crypto,"/serial"]),
+   Counter = <<"1000">>,
+   case file:read_file_info("cert/"++Crypto++"/synrc.cnf") of
+         {error,_} ->
+             filelib:ensure_dir(Gen),
+             lists:map(fun({A,B}) -> file:write_file(A,B) end,
+                [{Index,<<>>},{CRL,Counter},{Serial,Counter},{Gen,Bin}]),
+             ca(Crypto);
+         {ok,_} -> skip end,
+   {ok,man}.
+
+ca("rsa") ->
+    {done,0,_} = run("openssl genrsa -out cert/rsa/caroot.key 2048"),
+    {done,0,_} = run("openssl req -new -x509 -days 3650 -config cert/rsa/synrc.cnf"
+                         " -key cert/rsa/caroot.key -out cert/rsa/caroot.pem"
+                         " -subj \"/C=UA/ST=Kyiv/O=SYNRC/CN=CA\"");
+
+ca("ecc") ->
+    Pass = application:get_env(ca,passin,"pass:0"),
+    {done,0,Bin} = run("openssl ecparam -genkey -name secp384r1"),
+    file:write_file("cert/ecc/ca.key",Bin),
+    {done,0,_} = run("openssl ec -aes256 -in cert/ecc/ca.key -out cert/ecc/caroot.key -passout " ++ Pass),
+    {done,0,_} = run("openssl req -config cert/ecc/synrc.cnf -days 3650 -new -x509"
+        " -key cert/ecc/caroot.key -out cert/ecc/caroot.pem -passin " ++ Pass ++
+        " -subj \"/C=UA/ST=Kyiv/O=SYNRC/CN=CA\"").
 
 create(Crypto,Type) ->
     Pass = application:get_env(ca,passin,"pass:0"),
     Policy = case Type of "server" -> "server_cert"; "client" -> "usr_cert" end,
-    Args = "openssl ca -config cert/"++Crypto++"/synrc.cnf -extensions "++Policy++" -days 365"
-          " -in cert/"++Crypto++"/"++Type++".csr -out cert/"++Crypto++"/"++Type++".pem -passin "
-          ++ Pass ++ " -cert cert/"++Crypto++"/caroot.pem -keyfile cert/"++Crypto++"/caroot.key",
-    Port = erlang:open_port({spawn_executable, os:find_executable("sh")},
-        [stream, in, out, eof, use_stdio, stderr_to_stdout, binary, exit_status,
-            {args, ["-c",Args]}, {cd, element(2,file:get_cwd())}, {env, []}]),
-    X = sh(Port, fun({_, Chunk}, Acc) -> [Chunk|Acc] end, []),
-    X.
-
-sh(Port, Fun, Acc) ->
-    receive
-        {Port, {exit_status, Status}} -> {done, Status, iolist_to_binary(lists:reverse(Acc))};
-        {Port, {data, {eol, Line}}} -> sh(Port, Fun, Fun({eol, Line}, Acc));
-        {Port, {data, {noeol, Line}}} -> sh(Port, Fun, Fun({noeol, Line}, Acc));
-        {Port, {data, Data}} ->
-           case {binary:match(Data,  <<"Sign the certificate? [y/n]:">>) =/= nomatch,
-                 binary:match(Data,  <<"requests certified, commit?">>)  =/= nomatch} of
-                {true,_}   -> Port ! {self(),{command,<<"y\n">>}};
-                {_,true}   -> Port ! {self(),{command,<<"y\n">>}};
-                {_,_}      -> skip
-           end,
-           sh(Port, Fun, Fun({data,Data}, Acc))
-    end.
-
+    {done,0,_} = run("openssl ca -config cert/"++Crypto++"/synrc.cnf -extensions "++Policy++" -days 365"
+        " -in cert/"++Crypto++"/"++Type++".csr -out cert/"++Crypto++"/"++Type++".pem -passin "
+     ++ Pass ++ " -cert cert/"++Crypto++"/caroot.pem -keyfile cert/"++Crypto++"/caroot.key").
 
 init(Req0, Opts) ->
     Method = cowboy_req:method(Req0),
@@ -58,10 +75,8 @@ enroll(Crypto,Type,Req0)
     {_, DerCert, _} = CertEntry,
     Decoded = public_key:pkix_decode_cert(DerCert, otp),
     PKInfo = Decoded#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subjectPublicKeyInfo,
-    io:format("Log [~s,~s] ~p~n",[C,T,PKInfo]),
+    io:format("ENROLL REQUEST [~s,~s] ~p~n",[C,T,PKInfo]),
     echo(Cert, Req);
-enroll(_,_,R) -> echo(<<>>,R).
 
-echo(<<>>, Req) -> reply(400,#{},<<"General Enrolment Error">>,Req);
-echo(Echo, Req) -> reply(200,#{<<"content-type">> => <<"text/plain;charset=utf-8">>},Echo,Req).
-
+enroll(_,_,R) ->
+    echo(<<>>,R).
