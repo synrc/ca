fe63a0f3848771023688e60e958753c389bf7815
Namdak Tonpa <maxim@synrc.com>
2019-08-29 11:32:46 +0300

    v2

--- src/ca.erl
+++ src/ca.erl
@@ -7,6 +7,9 @@
 stop(_)    -> ok.
 init([])   -> {ok, { {one_for_one, 5, 10}, []} }.
 start(_,_) -> [ ca_enroll:boot(Crypto) || Crypto <- [ "rsa", "ecc" ] ],
-              R = cowboy_router:compile([{'_', [{"/:crypto/:nsCertType", ca_enroll, []}]}]),
+              R = cowboy_router:compile([{'_', [
+                  {"/up/:crypto", ca_up, []},
+                  {"/enroll/:crypto/:nsCertType", ca_enroll, []}
+              ]}]),
               {ok, _} = cowboy:start_clear(http,[{port,8046}],#{env => #{dispatch => R}}),
               supervisor:start_link({local, ?MODULE}, ?MODULE, []).
---
+++ src/ca_up.erl
@@ -0,0 +1,27 @@
+-module(ca_up).
+-include_lib("public_key/include/public_key.hrl").
+-compile(export_all).
+-export([init/2]).
+
+init(Req,Opts) ->
+    Method  = cowboy_req:method(Req),
+    HasBody = cowboy_req:has_body(Req),
+    Request = maybe_service(Method, HasBody, Req),
+    {ok, Request, Opts}.
+
+reply(X,Y,Z,R)  -> cowboy_req:reply(X,Y,Z,R).
+bind(X,Y)       -> binary_to_list(cowboy_req:binding(X,Y)).
+echo(<<>>, Req) -> reply(400,#{},<<"General CA Sync Error">>,Req);
+echo(Echo, Req) -> reply(200,#{<<"content-type">> => <<"text/plain;charset=utf-8">>},Echo,Req).
+
+maybe_service(<<"POST">>, true,  R) -> service(bind('crypto',R),R);
+maybe_service(<<"POST">>, false, R) -> reply(400, #{}, <<"Missing body.">>, R);
+maybe_service(_, _, R)              -> reply(405, #{}, <<"Unknown.">>, R).
+
+service(Crypto,Req) ->
+    {ok,PEM} = file:read_file("cert/"++Crypto++"/caroot.pem"),
+    {_,{_,D,_}} = lists:keysearch('Certificate',1,public_key:pem_decode(PEM)),
+    OTPCert = public_key:pkix_decode_cert(D,otp),
+    PKIInfo = OTPCert#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subjectPublicKeyInfo,
+    io:format("CA PUBLIC KEY [~s] ~p~n",[Crypto,PKIInfo]),
+    echo(PEM,Req).
