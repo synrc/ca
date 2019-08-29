e1c7699ac19737b06468e437a887620568d17c42
Namdak Tonpa <maxim@synrc.com>
2019-08-29 04:52:24 +0300

    cleanup

--- src/ca.erl
+++ src/ca.erl
@@ -1,14 +1,12 @@
 -module(ca).
--compile(export_all).
+-description('CA: Certificate Authority').
 -behaviour(application).
 -behaviour(supervisor).
 -export([start/2, stop/1, init/1]).
 
-start(_StartType, _StartArgs) ->
-   ca_enroll:boot("rsa"),
-   ca_enroll:boot("ecc"),
-   R = cowboy_router:compile([{'_', [{"/:crypto/:nsCertType", ca_enroll, []}]}]),
-   {ok, _} = cowboy:start_clear(http,[{port,8046}],#{env => #{dispatch => R}}),
-   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-stop(_State) -> ok.
-init([]) -> {ok, { {one_for_one, 5, 10}, []} }.
+stop(_)    -> ok.
+init([])   -> {ok, { {one_for_one, 5, 10}, []} }.
+start(_,_) -> [ ca_enroll:boot(Crypto) || Crypto <- [ "rsa", "ecc" ] ],
+              R = cowboy_router:compile([{'_', [{"/:crypto/:nsCertType", ca_enroll, []}]}]),
+              {ok, _} = cowboy:start_clear(http,[{port,8046}],#{env => #{dispatch => R}}),
+              supervisor:start_link({local, ?MODULE}, ?MODULE, []).
--- src/ca_enroll.erl
+++ src/ca_enroll.erl
@@ -1,82 +1,76 @@
 -module(ca_enroll).
+-copyright('Namdak Tonpa').
 -include_lib("public_key/include/public_key.hrl").
 -compile(export_all).
--export([init/2,create/2]).
+-export([init/2]).
 
+init(Req,Opts) ->
+    Method  = cowboy_req:method(Req),
+    HasBody = cowboy_req:has_body(Req),
+    Request = maybe_service(Method, HasBody, Req),
+    {ok, Request, Opts}.
+
+cat(X)          -> lists:concat(X).
 run(X)          -> sh:sh(sh:run(X)).
 reply(X,Y,Z,R)  -> cowboy_req:reply(X,Y,Z,R).
-bind(X,Y)       -> cowboy_req:binding(X,Y).
+bind(X,Y)       -> binary_to_list(cowboy_req:binding(X,Y)).
 template()      -> {ok,Bin} = file:read_file("priv/cnf/synrc.cnf"), Bin.
-replace(S,A,B)  -> re:replace(S,A,B,[global,{return,list}]).
+replace(S,A,B)  -> re:replace(S,A,B,[global,{return,binary}]).
 echo(<<>>, Req) -> reply(400,#{},<<"General Enrolment Error">>,Req);
 echo(Echo, Req) -> reply(200,#{<<"content-type">> => <<"text/plain;charset=utf-8">>},Echo,Req).
+root(Crypto)    -> {lists:concat(["cert/",Crypto,"/"]),"synrc.cnf"}.
 
 boot(Crypto) ->
-   Temp    = template(),
-   Tem2    = replace(Temp,"PATH", mad_utils:cwd()),
-   Bin     = iolist_to_binary(replace(Tem2,"CRYPTO",Crypto)),
-   Gen     = lists:concat(["cert/",Crypto,"/synrc.cnf"]),
-   Index   = lists:concat(["cert/",Crypto,"/index.txt"]),
-   CRL     = lists:concat(["cert/",Crypto,"/crlnumber"]),
-   Serial  = lists:concat(["cert/",Crypto,"/serial"]),
-   Counter = <<"1000">>,
-   case file:read_file_info("cert/"++Crypto++"/synrc.cnf") of
-         {error,_} ->
-             filelib:ensure_dir(Gen),
-             lists:map(fun({A,B}) -> file:write_file(A,B) end,
-                [{Index,<<>>},{CRL,Counter},{Serial,Counter},{Gen,Bin}]),
-             ca(Crypto);
-         {ok,_} -> skip end,
-   {ok,man}.
+   {Dir,CNF} = root(Crypto),
+   case file:read_file_info(Dir++CNF) of
+        {error,_} -> do_boot(Crypto);
+        {ok,_} -> skip end, {ok,Crypto}.
+
+do_boot(Crypto) ->
+   Num = <<"1000">>,
+   Bin = replace(replace(template(),"PATH",mad_utils:cwd()),"CRYPTO",Crypto),
+   {Dir,CNF} = root(Crypto),
+   filelib:ensure_dir(Dir),
+   lists:map(fun({A,B}) -> file:write_file(Dir++A,B) end,
+       [{"index.txt",<<>>},{"crlnumber",Num},{"serial",Num},{CNF,Bin}]),
+   ca(Crypto).
 
 ca("rsa") ->
     {done,0,_} = run("openssl genrsa -out cert/rsa/caroot.key 2048"),
     {done,0,_} = run("openssl req -new -x509 -days 3650 -config cert/rsa/synrc.cnf"
-                         " -key cert/rsa/caroot.key -out cert/rsa/caroot.pem"
-                         " -subj \"/C=UA/ST=Kyiv/O=SYNRC/CN=CA\"");
+       " -key cert/rsa/caroot.key -out cert/rsa/caroot.pem"
+       " -subj \"/C=UA/ST=Kyiv/O=SYNRC/CN=CA\"");
 
 ca("ecc") ->
     Pass = application:get_env(ca,passin,"pass:0"),
-    {done,0,Bin} = run("openssl ecparam -genkey -name secp384r1"),
-    file:write_file("cert/ecc/ca.key",Bin),
+    {done,0,_} = run("openssl ecparam -genkey -name secp384r1 -out cert/ecc/ca.key"),
     {done,0,_} = run("openssl ec -aes256 -in cert/ecc/ca.key -out cert/ecc/caroot.key -passout " ++ Pass),
     {done,0,_} = run("openssl req -config cert/ecc/synrc.cnf -days 3650 -new -x509"
         " -key cert/ecc/caroot.key -out cert/ecc/caroot.pem -passin " ++ Pass ++
         " -subj \"/C=UA/ST=Kyiv/O=SYNRC/CN=CA\"").
 
-create(Crypto,Type) ->
+enroll(CSR,Crypto,Type) ->
+    file:write_file(cat(["cert/",Crypto,"/",Type,".csr"]),CSR),
     Pass = application:get_env(ca,passin,"pass:0"),
     Policy = case Type of "server" -> "server_cert"; "client" -> "usr_cert" end,
     {done,0,_} = run("openssl ca -config cert/"++Crypto++"/synrc.cnf -extensions "++Policy++" -days 365"
         " -in cert/"++Crypto++"/"++Type++".csr -out cert/"++Crypto++"/"++Type++".pem -passin "
-     ++ Pass ++ " -cert cert/"++Crypto++"/caroot.pem -keyfile cert/"++Crypto++"/caroot.key").
+     ++ Pass ++ " -cert cert/"++Crypto++"/caroot.pem -keyfile cert/"++Crypto++"/caroot.key"),
+    file:read_file(cat(["cert/",Crypto,"/",Type,".pem"])).
 
-init(Req0, Opts) ->
-    Method = cowboy_req:method(Req0),
-    HasBody = cowboy_req:has_body(Req0),
-    Req = maybe_echo(Method, HasBody, Req0),
-    {ok, Req, Opts}.
+maybe_service(<<"POST">>, true,  R) -> service(bind('crypto',R),bind('nsCertType',R),R);
+maybe_service(<<"POST">>, false, R) -> reply(400, #{}, <<"Missing body.">>, R);
+maybe_service(_, _, R)              -> reply(405, #{}, <<"Unknown.">>, R).
 
-maybe_echo(<<"POST">>, true,  R) -> enroll(bind('crypto',R),bind('nsCertType',R),R);
-maybe_echo(<<"POST">>, false, R) -> reply(400, #{}, <<"Missing body.">>, R);
-maybe_echo(_, _, R)              -> reply(405, #{}, <<"Unknown.">>, R).
-
-enroll(Crypto,Type,Req0)
-    when (Crypto == <<"rsa">> orelse Crypto == <<"ecc">>) andalso
-         (Type == <<"client">> orelse Type == <<"server">>) ->
-    C = binary_to_list(Crypto),
-    T = binary_to_list(Type),
-    {ok, CSR, Req} = cowboy_req:read_body(Req0),
-    file:write_file("cert/"++C++"/"++T++".csr",CSR),
-    create(C,T),
-    {ok,Cert} = file:read_file("cert/"++C++"/"++T++".pem"),
-    Entries = public_key:pem_decode(Cert),
-    {value, CertEntry} = lists:keysearch('Certificate', 1, Entries),
-    {_, DerCert, _} = CertEntry,
-    Decoded = public_key:pkix_decode_cert(DerCert, otp),
-    PKInfo = Decoded#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subjectPublicKeyInfo,
-    io:format("ENROLL REQUEST [~s,~s] ~p~n",[C,T,PKInfo]),
+service(Crypto,Type,Req) when (Crypto == "rsa" orelse Crypto == "ecc")
+                      andalso (Type == "client" orelse Type == "server") ->
+    {ok,CSR, _} = cowboy_req:read_body(Req),
+    {ok, Cert } = enroll(CSR,Crypto,Type),
+    {_,{_,D,_}} = lists:keysearch('Certificate',1,public_key:pem_decode(Cert)),
+    OTPCert = public_key:pkix_decode_cert(D,otp),
+    PKIInfo = OTPCert#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subjectPublicKeyInfo,
+    io:format("ENROLL PUBLIC KEY [~s,~s] ~p~n",[Crypto,Type,PKIInfo]),
     echo(Cert, Req);
 
-enroll(_,_,R) ->
+service(_,_,R) ->
     echo(<<>>,R).
