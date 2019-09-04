e27322519461437226885dfd99f6b081bc801d02
Namdak Tonpa <maxim@synrc.com>
2019-09-04 16:32:45 +0300

    0.9.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CA.Mixfile do
   def project() do
     [
       app: :ca,
-      version: "0.8.2",
+      version: "0.9.0",
       elixir: "~> 1.7",
       description: "CA Certificate Authority",
       package: package(),
