f72c876bc248c7bbeb3823c77c8eec5e500cf9ae
5HT <maxim@synrc.com>
2023-06-14 07:40:03 +0300

    ecc script

--- priv/cnf/client-3.sh
+++ priv/cnf/client-3.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export CLIENT=client
-openssl pkcs12 -export \
-         -passin pass:0 -passout pass:0 \
-         -inkey cert/ecc/$CLIENT.key \
-         -in cert/ecc/$CLIENT.pem \
-         -out cert/ecc/$CLIENT.p12
+openssl pkcs12 -export -passin pass:0 -passout pass:0 \
+               -inkey cert/ecc/$CLIENT.key -in cert/ecc/$CLIENT.pem \
+               -out cert/ecc/$CLIENT.p12
--- priv/cnf/server-3.sh
+++ priv/cnf/server-3.sh
@@ -4,4 +4,4 @@ export SERVER=server
 openssl ca -config cert/ecc/synrc.cnf -days 730 -batch \
            -in cert/ecc/$SERVER.csr -out cert/ecc/$SERVER.pem \
            -keyfile cert/ecc/caroot.key -cert cert/ecc/caroot.pem \
-           -passin pass:0 -extensions server_cert \
+           -passin pass:0 -extensions server_cert
