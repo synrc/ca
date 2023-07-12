988ee70ae4275485383c18c6c3c0aedfd4898b02
5HT <maxim@synrc.com>
2023-07-12 18:10:47 +0300

    decryptCMS

--- lib/sec.ex
+++ lib/sec.ex
@@ -7,21 +7,51 @@ defmodule CA.CRYPTO do
     def shared(pub, key, scheme), do: :crypto.compute_key(:ecdh, pub, key, scheme)
     def eccCMS(ukm, len), do: {:'ECC-CMS-SharedInfo', {:'KeyWrapAlgorithm',{2,16,840,1,101,3,4,1,45},:asn1_NOVALUE}, ukm, <<len::32>>}
 
+    def decryptCMS(cms, privateKey) do
+        {:'ECPrivateKey',_,privateKeyBin,{:namedCurve,schemeOID},_,_} = privateKey
+        {_,{:ContentInfo,_,{:EnvelopedData,_,_,x,y,_}}} = testSMIME()
+        {:EncryptedContentInfo,_,{_,encOID,{_,iv}},data} = y
+        [{:kari,{_,:v3,{_,{_,_,publicKey}},ukm,{_,kdfOID,_},[{_,_,encryptedKey}]}}|_] = x
+        {scheme,_}  = CA.ALG.lookup(schemeOID)
+        {kdf,_}     = CA.ALG.lookup(kdfOID)
+        {enc,_}     = CA.ALG.lookup(encOID)
+        sharedKey   = shared(publicKey,privateKeyBin,scheme)
+        {_,payload} =  :'CMSECCAlgs-2009-02'.encode(:'ECC-CMS-SharedInfo', eccCMS(ukm, 256))
+        derivedKDF  = case kdf do
+           :'dhSinglePass-stdDH-sha512kdf-scheme' -> KDF.derive(:sha512, sharedKey, 32, payload)
+        end
+        unwrap = :aes_kw.unwrap(encryptedKey, derivedKDF)
+        case enc do
+           :'id-aes256-CBC' -> CA.AES.decrypt(:aes_256_cbc, data, unwrap, :binary.part(iv,2,16))
+        end
+    end
+
+    def testDecryptCMS() do
+        cms = testSMIME()
+        {privateKey,_} = testPrivateKey()
+        :io.format 'CMS: ~p~n', [cms]
+        :io.format 'PrivateKey: ~p~n', [privateKey]
+        decryptCMS(cms, privateKey)
+    end
+
     def testSMIME() do
         {:ok,base} = :file.read_file "priv/encrypted.txt" ; [_,s] = :string.split base, "\n\n"
         x = :base64.decode(s)
         :file.write_file "priv/encrypted.bin", [x]
-        y = :'CryptographicMessageSyntax-2010'.decode(:ContentInfo, x)
-        :io.format '~p~n', [y]
-        y
+        :'CryptographicMessageSyntax-2010'.decode(:ContentInfo, x)
+    end
+
+    def testPrivateKey() do
+        privateKey = :public_key.pem_entry_decode(readPEM("priv/certs/","maxim.key"))
+        privateKeyBin = e(3, privateKey)
+        {privateKey,privateKeyBin}
     end
 
     def testCMS() do
-        privateKey = privat "maxim.key"
-        :io.format '~p~n', [:public_key.pem_entry_decode(readPEM("priv/certs/","maxim.key"))]
+        {_,privateKey} = testPrivateKey()
         scheme = :secp256r1 # prime256v1
         {_,{:ContentInfo,_,{:EnvelopedData,_,_,x,{:EncryptedContentInfo,_,{_,_,{_,iv}},data},_}}} = testSMIME()
-        [{:kari,{_,:v3,{_,{_,_,publicKey}},ukm,_,[{_,_,encryptedKey}]}}|_] = x
+        [{:kari,{_,:v3,{_,{_,_,publicKey}},ukm,{_,_,_},[{_,_,encryptedKey}]}}|_] = x
         sharedKey    = shared(publicKey,privateKey,scheme)
         {_,content}  =  :'CMSECCAlgs-2009-02'.encode(:'ECC-CMS-SharedInfo', eccCMS(ukm, 256))
         kdf          = KDF.derive(:sha512, sharedKey, 32, content)
