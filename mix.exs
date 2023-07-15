8ae0bbc6f093a6add75d3feef2236af6f3d22d0f
5HT <maxim@synrc.com>
2023-07-16 02:11:18 +0300

    ca.cms

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CA.Mixfile do
   def project() do
     [
       app: :ca,
-      version: "4.7.13",
+      version: "4.7.14",
       elixir: "~> 1.7",
       description: "CA  CXC 138 21 Certificate Authority",
       package: package(),
