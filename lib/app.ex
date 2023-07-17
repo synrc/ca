19040109aa1390d34fe9ba5323777d1684b3c96b
5HT <maxim@synrc.com>
2023-07-17 03:46:38 +0300

    CA

--- lib/app.ex
+++ lib/app.ex
@@ -11,7 +11,7 @@ defmodule CA do
   def init([]), do: {:ok, { {:one_for_one, 5, 10}, []} }
   def port(), do: :application.get_env(:ca, :port, 8046)
   def start(_type, _args) do
-    {:ok, _} = :cowboy.start_clear(:http,[{:port,port()}],%{env: %{dispatch: :ca_enroll.boot()}})
+#    {:ok, _} = :cowboy.start_clear(:http,[{:port,port()}],%{env: %{dispatch: :ca_enroll.boot()}})
     :supervisor.start_link({:local, __MODULE__}, __MODULE__, [])
   end
 end
