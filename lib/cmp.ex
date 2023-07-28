1b524c0b99356db6764413c20499b7574aa3103b
5HT <maxim@synrc.com>
2023-07-28 18:44:59 +0300

    pbkdf2

--- lib/cmp.ex
+++ lib/cmp.ex
@@ -14,7 +14,6 @@ defmodule CA.CMP do
     #             -path . -srvcert ca.pem -ref cmptestp10cr \
     #             -secret pass:0000 -certout $client.pem -csr $client.csr
 
-
     def code(),         do: :binary.encode_hex(:crypto.strong_rand_bytes(8))
     def start(), do: :erlang.spawn(fn -> listen(1829) end)
 
@@ -26,7 +25,7 @@ defmodule CA.CMP do
 
     def accept(socket) do
         {:ok, fd} = :gen_tcp.accept(socket)
-        :erlang.spawn(fn -> loop(fd) end)
+        :erlang.spawn(fn -> __MODULE__.loop(fd) end)
         accept(socket)
     end
 
@@ -37,7 +36,6 @@ defmodule CA.CMP do
     end
 
     def unsubj({:rdnSequence, attrs}) do
-        :io.format '~p~n', [attrs]
         {:rdnSequence, :lists.map(fn [{t,oid,x}] -> case :asn1rt_nif.decode_ber_tlv(x) do
                                          {{12,a},_} -> [{t,oid,{:uTF8String,a}}]
                                          {{19,a},_} -> [{t,oid,:erlang.binary_to_list(a)}]
@@ -50,39 +48,73 @@ defmodule CA.CMP do
         {:Certificate,{:TBSCertificate,:v3,a,ai,unsubj(rdn),v,unsubj(rdn2),{p1,{p21,p22,{:namedCurve,{1,3,132,0,34}}},p3},b,c,ext},ai,code}
     end
 
-    def message(socket, header, {:p10cr, csr}, code) do
+    def mac(bin, salt, iter) do
+        base_key = :lists.foldl(fn x, acc ->
+            :crypto.hash(:sha256, acc) end, bin <> salt, :lists.seq(1,iter))
+        :binary.part(base_key, 0, 20)
+    end
+
+    def message(socket, header, {:p10cr, csr} = body, code) do
+        {:PKIHeader, pvno, from, to, messageTime, {x,oid,{y,param}} = protectionAlg, senderKID, recipKID,
+           transactionID, senderNonce, recipNonce, freeText, generalInfo} = header
+        {:ok, parameters} = :"PKIXCMP-2009".decode(:'PBMParameter', param)
+        {:PBMParameter, salt, {_,owf,_}, counter, {_,mac,_} } = parameters
         {:CertificationRequest, {:CertificationRequestInfo, v, subj, x, y}, b, c} = csr
         csr2 = {:CertificationRequest, {:CertificationRequestInfo, v, subj(subj), x, y}, b, c}
+
         {ca_key, ca} = CA.CSR.read_ca()
         subject = X509.CSR.subject(csr)
-#        :logger.info 'CA: ~p~n', [convertOTPtoPKIX(ca)]
-#        :logger.info 'CSR 2: ~p~n', [csr2]
         true = X509.CSR.valid?(csr2)
-#        :logger.info 'Subject: ~p~n', [subj(subject)]
-#        :logger.info 'Piblic Derive: ~p~n', [X509.CSR.public_key(csr2)]
         cert = X509.Certificate.new(X509.CSR.public_key(csr), subj(subject), ca, ca_key,
-         extensions: [subject_alt_name:
-           X509.Certificate.Extension.subject_alt_name(["n2o.dev", "synrc.com"]) ])
-        :io.format 'Issued Cert OTP: ~p~n', [cert]
-        pkix = convertOTPtoPKIX(cert)
-        :io.format 'Issued Cert PKIX: ~p~n', [pkix]
-        body = CA."CertRepMessage"(response:
-               [CA."CertResponse"(certReqId: 1,
-                   certifiedKeyPair: CA."CertifiedKeyPair"(certOrEncCert: {:certificate,{:x509v3PKCert,pkix}}),
-                   status: CA."PKIStatusInfo"(status: 1))])
-        certRepMessage = :'PKIXCMP-2009'.encode(:'CertRepMessage', body)
-        recipient = CA."PKIHeader"(header, :recipient)
-        sender = CA."PKIHeader"(header, :sender)
-        senderKID = CA."PKIHeader"(header, :senderKID)
-        recipientKID = CA."PKIHeader"(header, :senderKID)
-#        protection = CA."PKIHeader"(header, :protection)
-        {_,protectionAlg,_} = CA."PKIHeader"(header, :protectionAlg)
-        :io.format 'Protection: ~p~n', [code]
-        :io.format 'SenderKID: ~p~n', [senderKID]
-        :io.format 'RecipientKID: ~p~n', [recipientKID]
-        :io.format 'Protection Alg: ~p~n', [CA.ALG.lookup(protectionAlg)]
-        pkiheader = CA."PKIHeader"(sender: recipient, recipient: sender, pvno: :cmp2000)
-        answer(socket, pkiheader, {:cp,body}, code)
+           extensions: [subject_alt_name: X509.Certificate.Extension.subject_alt_name(["synrc.com"]) ])
+
+        reply = CA."CertRepMessage"(response:
+              [ CA."CertResponse"(certReqId: 1,
+                certifiedKeyPair: CA."CertifiedKeyPair"(certOrEncCert:
+                  {:certificate, {:x509v3PKCert, convertOTPtoPKIX(cert)}}),
+                status: CA."PKIStatusInfo"(status: 0))])
+
+        :io.format 'code: ~p~n', [code]
+
+        incomingProtection = CA."ProtectedPart"(header: header, body: body)
+        {:ok, bin} = :"PKIXCMP-2009".encode(:'ProtectedPart', incomingProtection)
+        verify1 = :crypto.pbkdf2_hmac(:sha256, bin, salt, counter, 20)
+        verify2 = mac(bin, salt, counter)
+        :io.format 'vfy1: ~p~n', [verify1]
+        :io.format 'vfy2: ~p~n', [verify2]
+        :io.format 'pvno: ~p~n', [pvno]
+#       :io.format 'from: ~p~n', [from]
+#       :io.format 'to: ~p~n', [to]
+        :io.format 'messageTime: ~p~n', [messageTime]
+        :io.format 'protectionAlgortihm: ~p~n', [CA.ALG.lookup(oid)]
+        :io.format 'senderKID: ~p~n', [senderKID]
+        :io.format 'recipKID: ~p~n', [recipKID]
+        :io.format 'transactionID: ~p~n', [transactionID]
+        :io.format 'senderNonce: ~p~n', [senderNonce]
+        :io.format 'recipNonce: ~p~n', [recipNonce]
+        :io.format 'freeText: ~p~n', [freeText]
+        :io.format 'generalInfo: ~p~n', [generalInfo]
+        :io.format 'salt: ~p~n', [salt]
+        :io.format 'owf: ~p~n', [CA.ALG.lookup(owf)]
+        :io.format 'counter: ~p~n', [counter]
+        :io.format 'mac: ~p~n', [CA.ALG.lookup(mac)]
+#       :io.format 'issuedOTP: ~p~n', [cert]
+#       :io.format 'issuedPKIX: ~p~n', [convertOTPtoPKIX(cert)]
+
+        pkiheader  = CA."PKIHeader"(sender: to,
+                                    recipient: from,
+                                    pvno: pvno,
+                                    protectionAlg: protectionAlg,
+                                    messageTime: messageTime,
+                                    transactionID: transactionID)
+        pkibody    = {:cp, reply}
+        outgoingProtection = CA."ProtectedPart"(header: pkiheader, body: pkibody)
+        {:ok, bin} = :"PKIXCMP-2009".encode(:'ProtectedPart', outgoingProtection)
+#       kdf = :crypto.pbkdf2_hmac(:sha256, bin, salt, counter, 20)
+        kdf = mac(bin, salt, counter)
+        :io.format 'protection: ~p~n', [kdf]
+
+        answer(socket, pkiheader, pkibody, kdf)
     end
 
     def answer(socket, header, body, code) do
@@ -101,9 +133,9 @@ defmodule CA.CMP do
                   {{_,headers},asn} = :asn1rt_nif.decode_ber_tlv(data)
                   [_,body] = :string.split asn, "\r\n\r\n", :all
                   {:ok,dec} = :'PKIXCMP-2009'.decode(:'PKIMessage', body)
-                  {:PKIMessage, header, body, code, _} = dec
-                  :io.format 'PKIMessage:~n~p~n', [dec]
-                  message(socket, header, body, code)
+                  {:PKIMessage, header, body, code, extra} = dec
+#                 :io.format 'PKIMessage:~n~p~n', [dec]
+                  __MODULE__.message(socket, header, body, code)
                   loop(socket)
              {:error, :closed} -> :exit
         end
