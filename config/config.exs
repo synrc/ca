e369983433fced01285c0ddfadaac0ae2f49eb7f
5HT <maxim@synrc.com>
2023-07-28 20:46:00 +0300

    maintatance

---
+++ config/config.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+config :ca,
+  ldap_port: 1389,
+  cmp_port: 829,
+  ocsp_port: 1000,
+  tsp_port: 1001,
+  ca_port: 8088,
+  logger_level: :info,
+  logger: [{:handler, :default, :logger_std_h,
+            %{level: :info,
+              id: :synrc,
+              max_size: 2000,
+              module: :logger_std_h,
+              config: %{type: :file, file: 'ca.log'},
+              formatter: {:logger_formatter,
+                          %{template: [:time,' ',:pid,' ',:module,' ',:msg,'\n'],
+                            single_line: true,}}}}]
+
+
---
+++ priv/csr/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm *.asn1db *.beam *.erl *.hrl
