b52f1edeb934576f57f9238ef42fb3c5a2c2f1d4
5HT <maxim@synrc.com>
2023-07-31 16:34:38 +0300

    secret

--- p10cr.sh
+++ p10cr.sh
@@ -4,4 +4,4 @@ export client=maxim
 
 openssl cmp -cmd p10cr -server localhost:1829 \
             -path . -srvcert ca.pem -ref cmptestp10cr \
-            -secret pass:0000 -certout $client.pem -csr $client.csr
+            -secret pass:ThisIsClassified -certout $client.pem -csr $client.csr
