c58fbbe721c732427a5e166f9854b0a57c03c603
5HT <maxim@synrc.com>
2024-02-21 19:10:00 +0200

    beautify

--- lib/CAdES.ex
+++ lib/CAdES.ex
@@ -1,9 +1,8 @@
 defmodule CA.CAdES do
-  @moduledoc "CAdES (804) Qualified Digital Signature library ."
+  @moduledoc "CAdES (804) Qualified Digital Signature library."
   require Record
-  Record.defrecord(:certAttrs, serial: "", cn: "", givenName: "", surname: "",
-                               timeStamp: "",
-                               o: "", title: "", ou: "", c: "", locality: "")
+  Record.defrecord(:certinfo, serial: "", cn: "", givenName: "", surname: "", timeStamp: "",
+                              attrs: [], o: "", title: "", ou: "", c: "", locality: "")
 
   def subj({:rdnSequence, attrs}) do
         {:rdnSequence, :lists.map(fn
@@ -22,8 +21,7 @@ defmodule CA.CAdES do
              end end, attrs)}
   end
 
-  def readSignature() do
-      name = "signature001.p7s"
+  def readSignature(name \\ "2.p7s") do
       {:ok, bin} = :file.read_file name
       ber = parseSignData(bin)
       ber
@@ -38,17 +36,16 @@ defmodule CA.CAdES do
   end
 
   def parseSignData(bin) do
-      {_, {:ContentInfo, x, ci}} = :KEP.decode(:ContentInfo, bin)
+      {_, {:ContentInfo, oid, ci}} = :KEP.decode(:ContentInfo, bin)
       {:ok, {:SignedData, _, alg, x, c, x1, si}} = :KEP.decode(:SignedData, ci)
-      parseSignDataCert({alg,x,c,x1,si})
+      parseSignDataCert({alg,oid,x,c,x1,si})
   end
 
-  def parseSignDataCert({_,_,:asn1_NOVALUE,_,_}), do: []
-  def parseSignDataCert({_,_,[cert],_,attr}), do: parseCert(cert,attr)
+  def parseSignDataCert({_,_,_,:asn1_NOVALUE,_,_}), do: []
+  def parseSignDataCert({_,_,_,[cert],_,attr}), do: parseCert(cert,attr)
 
-  def parseAttrs(attrs) do
-      :io.format 'Attr: ~p~n', [attrs]
-      certAttrs(
+  def parseAttrs(attrs, options) do
+      certinfo(
         o: extract({2, 5, 4, 10}, attrs),
         ou: extract({2, 5, 4, 11}, attrs),
         title: extract({2, 5, 4, 12}, attrs),
@@ -58,32 +55,52 @@ defmodule CA.CAdES do
         surname: extract({2, 5, 4, 4}, attrs),
         locality: extract({2, 5, 4, 7}, attrs),
         serial: extract({2, 5, 4, 5}, attrs),
-        c: extract({2, 5, 4, 6}, attrs))
+        c: extract({2, 5, 4, 6}, attrs),
+        attrs: options)
   end
 
   def pair([],acc), do: acc
   def pair([x],acc), do: [x|acc]
-  def pair([a,b|t],acc), do: pair(t,[{:oid.decode(a),b}|acc])
+  def pair([a,b|t],acc), do: pair(t,[{hd(mapOids([:oid.decode(a)])),b}|acc])
+
+  def mapOidsDecode(list) do
+      :lists.map(fn x ->
+         :erlang.iolist_to_binary(:string.join(:lists.map(fn y -> :erlang.integer_to_list(y) end,
+         :erlang.tuple_to_list(:oid.decode(x))),'.')) end, list)
+  end
+
+  def mapOids(list) do
+      :lists.map(fn x ->
+         :erlang.iolist_to_binary(:string.join(:lists.map(fn y -> :erlang.integer_to_list(y) end,
+         :erlang.tuple_to_list(x)),'.')) end, list)
+  end
 
   def oid({1,3,6,1,5,5,7,1,1}, v), do: {:authorityInfoAccess, pair(v,[])}
-  def oid({1,3,6,1,5,5,7,1,3}, [v]), do: {:qcStatements, :oid.decode v}
+  def oid({1,3,6,1,5,5,7,1,3}, [v1,v2,url,_lang,v3,v4,v5]), do: {:qcStatements, {url, mapOidsDecode([v1,v2,v3,v4,v5])}}
   def oid({1,3,6,1,5,5,7,1,11},v), do: {:subjectInfoAccess, pair(v,[])}
   def oid({2,5,29,9},v),  do: {:subjectDirectoryAttributes, pair(v,[])}
-  def oid({2,5,29,14},v), do: {:subjectKeyIdentifier, pair(v,[])}
+  def oid({2,5,29,14},v), do: {:subjectKeyIdentifier, :base64.encode(hd(pair(v,[])))}
   def oid({2,5,29,15},v), do: {:keyUsage, pair(v,[])}
   def oid({2,5,29,16},v), do: {:privateKeyUsagePeriod, v}
   def oid({2,5,29,17},v), do: {:subjectAltName, pair(v,[])}
   def oid({2,5,29,19},v), do: {:basicConstraints, pair(v,[])}
   def oid({2,5,29,31},v), do: {:cRLDistributionPoints, pair(v,[])}
-  def oid({2,5,29,32},[v]), do: {:certificatePolicies, :oid.decode v}
-  def oid({2,5,29,35},v), do: {:authorityKeyIdentifier, pair(v,[])}
+  def oid({2,5,29,32},[v1,v2,url]), do: {:certificatePolicies, {url, mapOidsDecode([v1,v2])}}
+  def oid({2,5,29,35},v), do: {:authorityKeyIdentifier, :base64.encode(hd(pair(v,[])))}
   def oid({2,5,29,46},v), do: {:freshestCRL, pair(v,[])}
-  def oid({2,5,29,46},v), do: {:freshestCRL, pair(v,[])}
-  def oid({1,2,840,113549,1,9,3},v), do: {:contentType, :oid.decode(v)}
-  def oid({1,2,840,113549,1,9,4},v), do: {:messageDigest, :asn1rt_nif.decode_ber_tlv v}
-  def oid({1,2,840,113549,1,9,5},v), do: {:signingTime, :asn1rt_nif.decode_ber_tlv(v)}
-  def oid({1,2,840,113549,1,9,16,2,47},v), do: {:signingCertificateV2, :erlang.element(2,:KEP.decode(:AttributeValue, v))}
-  def oid({1,2,840,113549,1,9,16,2,20},v), do: {:contentTimestamp, :erlang.element(2,:KEP.decode(:AttributeValue, v)) }
+  def oid({1,2,840,113549,1,9,3},v), do: {:contentType, hd(mapOidsDecode([v]))}
+  def oid({1,2,840,113549,1,9,4},v), do: {:messageDigest, :base64.encode(:erlang.element(2,:KEP.decode(:MessageDigest, v)))}
+  def oid({1,2,840,113549,1,9,5},v), do: {:signingTime, :erlang.element(2,:erlang.element(1,:asn1rt_nif.decode_ber_tlv(v)))}
+  def oid({1,2,840,113549,1,9,16,2,47},v) do
+      {:SigningCertificateV2,[{:ESSCertIDv2, _, _, {_,_,serial}}],_} = :erlang.element(2,:KEP.decode(:SigningCertificateV2, v))
+      {:signingCertificateV2, serial}
+  end
+  def oid({1,2,840,113549,1,9,16,2,20},v) do
+      {:ContentInfo, oid, value} = :erlang.element(2,:KEP.decode(:ContentInfo,v))
+      {:ok, {:SignedData, _, _alg, {_,_,x}, _c, _x1, _si}} = :KEP.decode(:SignedData, value)
+      {:ok, {:TSTInfo, _vsn, _oid, {:MessageImprint, _, x}, serial, ts, _,_,_,_}} = :KEP.decode(:TSTInfo, x)
+      {:contentTimestamp, {hd(mapOids([oid])), serial, :erlang.iolist_to_binary(ts), :base64.encode(x)}}
+  end
   def oid(x,v) when is_binary(x), do: {:oid.decode(x),pair(v,[])}
   def oid(x,v), do: {x,v}
 
@@ -93,14 +110,6 @@ defmodule CA.CAdES do
   def flat(_code,k,acc) when is_binary(k), do: [k|acc]
 
   def parseCert(cert, [si|_]) do
-    :io.format '1 ~p ~n',[:erlang.element(1,si)]
-    :io.format '2 ~p ~n',[:erlang.element(2,si)]
-    :io.format '3 ~p ~n',[:erlang.element(3,si)]
-    :io.format '4 ~p ~n',[:erlang.element(4,si)]
-    :io.format '5 ~p ~n',[:erlang.element(5,si)]
-    :io.format '6 ~p ~n',[:erlang.element(6,si)]
-    :io.format '7 ~p ~n',[:erlang.element(7,si)]
-    :io.format '8 ~p ~n',[:erlang.element(8,si)]
     {:SignerInfo, _v, _serial, _alg, attrs, _, _, _} = si
     {:Certificate, a, _, _} = cert
     {:Certificate_toBeSigned, _ver, _sel, _alg, issuer, _val, issuee, _a, _b, _c, exts} = a
@@ -112,7 +121,18 @@ defmodule CA.CAdES do
       end, attrs)
     person = :lists.flatten(:erlang.element(2, issuee))
     ca = :lists.flatten(:erlang.element(2, issuer))
-    {parseAttrs(person),parseAttrs(ca),extensions,attributes}
+    [parseAttrs(person,extensions),parseAttrs(ca,attributes)]
+  end
+
+  def parseCert(cert) do
+    {:Certificate, a, _, _} = cert
+    {:Certificate_toBeSigned, _ver, _sel, _alg, issuer, _val, issuee, _a, _b, _c, exts} = a
+    extensions = :lists.map(fn {:Extension,code,_x,b} ->
+         oid(code, :lists.flatten(flat(code,:asn1rt_nif.decode_ber_tlv(b),[])))
+      end, exts)
+    person = :lists.flatten(:erlang.element(2, issuee))
+    ca = :lists.flatten(:erlang.element(2, issuer))
+    [parseAttrs(person,extensions),parseAttrs(ca,[])]
   end
 
 end
