823406228f10fa5685317eed15d85da5804ef551
5HT <maxim@synrc.com>
2024-10-23 20:58:53 +0300

    signing is consistent

--- lib/ecdsa.ex
+++ lib/ecdsa.ex
@@ -6,6 +6,7 @@ defmodule CA.ECDSA.OTP do
   # openssl ec -in $client.key -pubout -out $client.pub
   # openssl dgst -sha256 -sign $client.key mix.exs > mix.sig
   # openssl dgst -sha256 -verify $client.pub -signature mix.sig mix.exs
+  # CA.ECDSA.sign "mix.exs", "#{client}.key"
   # CA.ECDSA.verify "mix.exs", "mix.sig", "#{client}.pub"
   # CA.ECDSA.OTP.verify "mix.exs", "mix.sig", "#{client}.pub"
 
