18894e4af118fc78c67543a8cb1850fa62b537f7
5HT <maxim@synrc.com>
2024-10-28 12:14:08 +0200

    5.10.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CA.Mixfile do
   def project() do
     [
       app: :ca,
-      version: "5.10.2",
+      version: "5.10.3",
       description: "CA  CXC 138 21 Certificate Authority",
       package: [
         name: :ca,
