285731b6dbe8930fbf176a5a2f263f9bf4d6d8a0
5HT <maxim@synrc.com>
2024-10-28 11:49:59 +0200

    CA.NIS

--- lib/services/est.ex
+++ lib/services/est.ex
@@ -20,14 +20,16 @@ defmodule CA.EST do
 
   # Authority PKI X.509 EST RFC 7030 3.2.2
 
-  post "/.well-known/est/simpleenroll"   do CA.EST.Post.post(conn, [], "Authority", [], "ENROLL") end
-  post "/.well-known/est/simplereenroll" do CA.EST.Post.post(conn, [], "Authority", [], "RE-ENROLL") end
   get  "/.well-known/est/cacerts"        do CA.EST.Get.get(conn,   [], "Authority", [], "ROOT") end
   get  "/.well-known/est/csrattrs"       do CA.EST.Get.get(conn,   [], "Authority", [], "ABAC") end
-  post "/.well-known/est/fullcmc"        do CA.EST.Post.post(conn, [], "Authority", [], "CMC") end
+  post "/.well-known/est/simpleenroll"   do CA.EST.Post.post(conn, [], "Authority", [], "ENROLL") end
+  post "/.well-known/est/simplereenroll" do CA.EST.Post.post(conn, [], "Authority", [], "RE-ENROLL") end
   post "/.well-known/est/serverkeygen"   do CA.EST.Post.post(conn, [], "Authority", [], "KEYGEN") end
+  post "/.well-known/est/fullcmc"        do CA.EST.Post.post(conn, [], "Authority", [], "CMC") end
 
   # See Page 36 of RFC 7030
+  # [1] https://www.rfc-editor.org/rfc/rfc7030
+  # [2] https://www.ietf.org/archive/id/draft-ietf-lamps-rfc7030-csrattrs-07.html
 
   def csrattributes() do
       {:ok, bin} = :"EST".encode(:CsrAttrs, [
