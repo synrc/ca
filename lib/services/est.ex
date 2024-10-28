c547c8aeec8cab62eef1853cc976485a15ac75b6
5HT <maxim@synrc.com>
2024-10-28 11:39:48 +0200

    separate folder for oids

--- lib/services/est.ex
+++ lib/services/est.ex
@@ -18,13 +18,14 @@ defmodule CA.EST do
       Supervisor.start_link(children, strategy: :one_for_one, name: CA.Supervisor)
   end
 
-  # Authority PKI X.509 EST RFC 7030
+  # Authority PKI X.509 EST RFC 7030 3.2.2
 
   post "/.well-known/est/simpleenroll"   do CA.EST.Post.post(conn, [], "Authority", [], "ENROLL") end
   put  "/.well-known/est/simplereenroll" do CA.EST.Put.put(conn,   [], "Authority", [], "RE-ENROLL") end
   get  "/.well-known/est/cacerts"        do CA.EST.Get.get(conn,   [], "Authority", [], "ROOT") end
   get  "/.well-known/est/csrattrs"       do CA.EST.Get.get(conn,   [], "Authority", [], "ABAC") end
   put  "/.well-known/est/fullcmc"        do CA.EST.Put.put(conn,   [], "Authority", [], "CMC") end
+  post "/.well-known/est/serverkeygen"   do CA.EST.Post.put(conn,  [], "Authority", [], "KEYGEN") end
 
   # See Page 36 of RFC 7030
 
