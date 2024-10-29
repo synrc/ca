7f7e78ec05635dc7fe26d92689db9bd9aacdd35c
5HT <maxim@synrc.com>
2024-10-29 18:12:02 +0200

    curve oids

--- lib/agreement/EC/curve.ex
+++ lib/agreement/EC/curve.ex
@@ -3,6 +3,21 @@ defmodule CA.Curve do
   require CA.Integer
   require CA.Point
   defstruct [:A, :B, :P, :N, :G, :name, :oid]
+
+  def oid(:sect163k1), do: {1,3,132,0,1}
+  def oid(:sect163r2), do: {1,3,132,0,15}
+  def oid(:secp224r1), do: {1,3,132,0,33}
+  def oid(:sect233k1), do: {1,3,132,0,26}
+  def oid(:sect233r1), do: {1,3,132,0,27}
+  def oid(:sect283k1), do: {1,3,132,0,16}
+  def oid(:sect283r1), do: {1,3,132,0,17}
+  def oid(:secp384r1), do: {1,3,132,0,34}
+  def oid(:sect409k1), do: {1,3,132,0,36}
+  def oid(:sect409r1), do: {1,3,132,0,37}
+  def oid(:secp521r1), do: {1,3,132,0,35}
+  def oid(:sect571k1), do: {1,3,132,0,38}
+  def oid(:sect571r1), do: {1,3,132,0,39}
+
   def contains?(curve, p) do
       cond do
         p.x < 0 || p.x > curve."P" - 1 -> false
--- lib/oid/x962.ex
+++ lib/oid/x962.ex
@@ -5,6 +5,16 @@ defmodule CA.X962 do
   def oid(:"id-kt-ecPublicKey"),                          do: {1, 2, 840, 10045, 2, 1}
   def oid(:"id-ct-characteristicTwo"),                    do: {1, 2, 840, 10045, 3, 0}
   def oid(:"id-ct-prime"),                                do: {1, 2, 840, 10045, 3, 1}
+  def oid(:ansiX9p192r1),                                 do: {1, 2, 840, 10045, 3, 1, 1}
+  def oid(:secp192r1),                                    do: {1, 2, 840, 10045, 3, 1, 1}
+  def oid(:prime192v1),                                   do: {1, 2, 840, 10045, 3, 1, 1}
+  def oid(:prime192v2),                                   do: {1, 2, 840, 10045, 3, 1, 2}
+  def oid(:prime192v3),                                   do: {1, 2, 840, 10045, 3, 1, 3}
+  def oid(:prime239v1),                                   do: {1, 2, 840, 10045, 3, 1, 4}
+  def oid(:prime239v2),                                   do: {1, 2, 840, 10045, 3, 1, 5}
+  def oid(:prime239v3),                                   do: {1, 2, 840, 10045, 3, 1, 6}
+  def oid(:secp256r1),                                    do: {1, 2, 840, 10045, 3, 1, 7}
+  def oid(:prime256v1),                                   do: {1, 2, 840, 10045, 3, 1, 7}
   def oid(:"id-ds-ecdsa-with-SHA1"),                      do: {1, 2, 840, 10045, 4, 1}
   def oid(:"id-ds-ecdsa-with-Recommended"),               do: {1, 2, 840, 10045, 4, 2}
   def oid(:"id-ds-ecdsa-with-SHA2"),                      do: {1, 2, 840, 10045, 4, 3}
