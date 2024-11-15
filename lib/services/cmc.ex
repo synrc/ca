608affb9c440d109387753d6b61ba49614a4a83e
5HT <maxim@synrc.com>
2024-11-15 04:42:32 +0200

    cmc

--- lib/services/cmc.ex
+++ lib/services/cmc.ex
@@ -35,7 +35,7 @@ defmodule CA.CMC do
 
   def code(),  do: :binary.encode_hex(:crypto.strong_rand_bytes(8))
 
-  # Authority PKI X.509 CMC over HTTP RFC 5272 5273 5274 5275 6402
+  # Authority PKI X.509 CMC over TCP RFC 5272 5273 5274 5275 6402
 
   # [2] https://www.rfc-editor.org/rfc/rfc5272
   # [2] https://www.rfc-editor.org/rfc/rfc5273
