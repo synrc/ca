e8fe0c5c4e96cdb7f09f00509ba1584bc8767b38
Namdak Tonpa <maxim@synrc.com>
2026-06-17 02:57:05 +0300

    cmdb

--- priv/run.sh
+++ priv/run.sh
@@ -4,8 +4,11 @@ pdflatex legal_l1_profile_419.tex
 pdflatex legal_l2_court_profile.tex
 pdflatex legal_l3_profile_erp.tex
 
-pdflatex security-profiles.tex
+pdflatex nist.tex
+pdflatex security-policy.tex
+pdflatex security-architecture.tex
 pdflatex bible_profile.tex
+
 pdflatex chat_profile.tex
 pdflatex mail_profile.tex
 pdflatex vpn_profile.tex
